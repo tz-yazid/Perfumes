48f2544929df3a78da9498985dfc43555b06b59e
ranyafazai <fazaeiranya@gmail.com>
2025-01-09 22:15:56 +0100

    check work

--- scripts/login.js
+++ scripts/login.js
@@ -7,20 +7,6 @@ function Login(name,password){
 var name = $("username").val()
 var password = $("pass").val()
 var newuser = Login(name,password)
-<<<<<<< HEAD
-var button= document.getElementById("butt")
-
- button.addEventListener("click",test)
- function test(){
-   if (newuser.name ==="" || newuser.password==="") {
-      alert('check your name and password');
- }
- else {
-   alert('login succesfully');
-   }
- }
-=======
->>>>>>> 20be0803c443360d22be8ab7a0f1af69ef46463e
 var array = [newuser]
 var add=function(newuser){
   array.push(newuser)
