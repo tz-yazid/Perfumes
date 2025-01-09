03a432b43e8fe79f768b3fb59e1ff6eaf220a948
tz-yazid <yazidtezi@gmail.com>
2025-01-09 12:58:22 +0100

    wait

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,5 +1,14 @@
-
-
+function MakeParfume(name,img){
+   var obj={}
+   obj.name=name,
+   obj.img=img
+   return obj
+}
+var result=MakeParfume("","")
+var array=[result]
+var img=function(){
+   
+}
 var search=function(array,query){
     for(var i=0; i<array.lentgh; i++){
        if(array[i].name === query){
