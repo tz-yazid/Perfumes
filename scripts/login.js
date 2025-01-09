2e31b2556b7620aa50183c521bdb24dd5a03a1d2
tz-yazid <yazidtezi@gmail.com>
2025-01-09 11:02:06 +0100

    hold

--- scripts/login.js
+++ scripts/login.js
@@ -1,35 +1,27 @@
 function Login(name,password){
    var obj={}
-   obj.name=name
+   obj.name=name,
    obj.password=password
    return obj
 }
-var newuser = Login('','')
-var array = [newuser]
-var name = document.getElementById("username")
-var password = document.getElementById("password")
+var name = document.getElementById("username").value
+var password = document.getElementById("pass").value
+var newuser = Login(name,password)
 var button= document.getElementById("butt")
  button.addEventListener("click",test)
  function test(){
-   if (this.name===null || this.password===null) {
-    return 'please enter a name and password'
+   if (newuser.name ==="" || newuser.password==="") {
+      alert('check your name name and password');
  }
  else {
-     return 'login succesfully'
-    }
+   alert('login succesfully');
+   }
  }
+ var array = [newuser]
 var add=function(newuser){
     array.push(newuser)
 }
-var search=function(array,query){
-for(var i=0; i<array.lentgh; i++){
-   if(array[i].name === query){
-      return query
-     }
-     }
-     return "undefined"
-}
-localStorage.setItem("users",array)
+localStorage.getItem("users",array)
 JSON.stringify(array)
 JSON.parse(array)
 
