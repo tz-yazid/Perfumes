ea5aa38194783bc8d2172a8309e9b963f40f646d
tz-yazid <yazidtezi@gmail.com>
2025-01-09 01:05:08 +0100

    done

--- scripts/login.js
+++ scripts/login.js
@@ -1,26 +1,35 @@
-var name= document.getElementById("username")
-var password= document.getElementById("password")
-var button= document.getElementById("butt")
-
-
-
-function login(name,password){
+function Login(name,password){
    var obj={}
    obj.name=name
    obj.password=password
-   obj.button=function(){
-    if (this.name===null || this.password===null) {
-       return 'please enter a name and password'
-    }
-    else { return 'login succesfully'}
-   }
    return obj
 }
+var newuser = Login('','')
 var array = [newuser]
-var newuser = login('','')
-
-function addUser(array,newuser){
+Login.prototype.button=function(){
+   if (this.name===null || this.password===null) {
+      return 'please enter a name and password'
+   }
+   else {
+       return 'login succesfully'
+      }
+  }
+ function test(){
+   var name = document.getElementById("username")
+  var password = document.getElementById("password")
+ }
+ var button= document.getElementById("butt")
+ button.addEventListener("click",test)
+var add=function(newuser){
     array.push(newuser)
+}
+var search=function(array,query){
+for(var i=0; i<array.lentgh; i++){
+   if(array[i].name === query){
+      return query
+     }
+     }
+     return "undefined"
 }
 
 localStorage.set
