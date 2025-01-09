1a6952e7c1af500362a024a41c63d1f29d16d169
tz-yazid <yazidtezi@gmail.com>
2025-01-09 13:51:33 +0100

    wait

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -4,14 +4,23 @@ function MakeParfume(name,img){
    obj.img=img
    return obj
 }
-var result=MakeParfume("","")
-var array=[result]
+var result=MakeParfume("","");
+var array=[result];
 var img=function(){
-   
+   img:"../photos/Burberry.jpg";
+   img:"../photos/Adidas.jpg";
+   img:"../photos/Britany Spears.jpg";
+   img:"../photos/Coco.jpg";
+   img:"../photos/Kilian.jpg";
+   img:"../photos/Miss Dior.jpg";
+   img:"../photos/Nike.jpg";
+   img:"../photos/Scorpion.jpg";
+   img:"../photos/Sior Maison.jpg";
+   img:"../photos/Valentino Uomo.jpg";
 }
 var search=function(array,query){
     for(var i=0; i<array.lentgh; i++){
-       if(array[i].name === query){
+       if(array[i].img === query){
           return query
          }
          }
