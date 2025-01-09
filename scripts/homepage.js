86330ea36a6856eec259a0a1452d02c84dd0a93f
tz-yazid <yazidtezi@gmail.com>
2025-01-09 15:30:28 +0100

    rest

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,28 +1,25 @@
-function MakeParfume(name,img){
-   var obj={}
-   obj.name=name,
-   obj.img=img
-   return obj
-}
-var result=MakeParfume("","");
-var array=[result];
-var img=function(){
-   img:"../photos/Burberry.jpg";
-   img:"../photos/Adidas.jpg";
-   img:"../photos/Britany Spears.jpg";
-   img:"../photos/Coco.jpg";
-   img:"../photos/Kilian.jpg";
-   img:"../photos/Miss Dior.jpg";
-   img:"../photos/Nike.jpg";
-   img:"../photos/Scorpion.jpg";
-   img:"../photos/Sior Maison.jpg";
-   img:"../photos/Valentino Uomo.jpg";
-}
-var search=function(array,query){
-    for(var i=0; i<array.lentgh; i++){
-       if(array[i].img === query){
-          return query
-         }
-         }
-         return "undefined"
-    }
+console.log("ok");
+var perfume=document.getElementById('perfumename')
+var imageperfume =document.getElementById('perfumeimg')
+
+
+var perfumes = [ { name: "adidas", img: "/photos/Adidas.jpg" }, { name: "burberry", img: "/photos/Burberry.jpg" }, { name: "britany", img: "/photos/Britany Spears.jpg" }, { name: "nike", img: "/photos/Nike.jpg" }, { name: "coco", img: "/photos/Coco.jpg" }, { name: "kilian", img: "/photos/Kilian.jpg" }, { name: "scorpion", img: "/photos/Scorpion.jpg" }, { name: "sior", img: "/photos/Sior Maison.jpg" }, { name: "valentino", img: "/photos/Valentino Uomo.jpg" } ];
+function getperfume(){
+for (let i = 0; i < pefumes.length; i++) {
+   perfume = perfumes[i].name
+   imageperfume=perfumes[i].img
+}}
+
+var reslult=$("#perfumeid")
+var button=$("#button")
+console.log(button,"buuton");
+function search (perfumes,result){
+   console.log("test")
+   for(var i=0; i<perfumes.lentgh; i++){
+      if(perfumes[i].name === result){
+         $("#div").append(perfumes[i]);
+        }
+        }
+        alert("undefined");
+   }
+   button.on("click", search);
